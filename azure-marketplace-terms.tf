304217e5b1a7876a9579517e63ee034dfb5aefaf
Jim Öqvist <44175368+jimoq@users.noreply.github.com>
2023-10-24 15:50:01 +0200

    Update azure-marketplace-terms.tf

--- azure-marketplace-terms.tf
+++ azure-marketplace-terms.tf
@@ -1,6 +1,6 @@
-# Allows accepting the Legal Terms for Marketplace agreements for the mgmt-byol 
+# Allows accepting the Legal Terms for Marketplace agreements for the sg-byol / mgmt-byol plan
 resource "azurerm_marketplace_agreement" "checkpoint" {
   publisher = "checkpoint"
-  offer     = "check-point-cg-r8110"
-  plan      = "mgmt-byol"
+  offer     = "check-point-cg-r81"
+  plan      = "sg-byol"
 }
