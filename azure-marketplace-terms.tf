474bbeee02f6a1aec245197208d44856cf5ea297
Jim Öqvist <44175368+jimoq@users.noreply.github.com>
2023-10-24 22:15:46 +0000

    use var fro version

--- azure-marketplace-terms.tf
+++ azure-marketplace-terms.tf
@@ -1,6 +1,6 @@
 # Allows accepting the Legal Terms for Marketplace agreements for the sg-byol / mgmt-byol plan
 resource "azurerm_marketplace_agreement" "checkpoint" {
   publisher = "checkpoint"
-  offer     = "check-point-cg-r81"
+  offer     = "check-point-cg-${var.os_version}"
   plan      = "sg-byol"
 }
